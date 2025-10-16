e6e49a392e200becae34f3a069783bec00a0a959
cauanfaveri <cauanfaveri05@gmail.com>
2025-10-16 08:57:47 -0300

    Fim do exercicio 1

--- server.js
+++ server.js
@@ -3,35 +3,37 @@ const app = express();
 const PORT = 8082;
 
 app.use(express.json())
-async function validaçao(pNotas) {
-    try {
-        if (isNaN(pNotas)) {
+
+async function validacaoNotas(pNotas) {
+    pNotas.forEach(nota => {
+        if (isNaN(nota)) {
             throw new Error("Os valores digitados são inválidos")
         }
-        const notas = parseFloat(pNotas);
-        return { notas };
-    } catch (error) {
-        throw new Error(error)
-    }
+    });
 }
 
 app.post('/alunos', async (req, res) => {
     try {
         const { nome, notas } = req.body;
         console.log(nome, notas);
-        const media = notas.reduce((accumulator, currentValue) => accumulator + currentValue)/4;
-        if (media > 6){
-            Aprovacao = `Você foi aprovada`
-        } else {
-            Aprovacao = `Voce foi reprovada`
-        }
-        res.status(201).json({ message: `Ola ${nome}, Sua média é ${media}, ${Aprovacao}`});    
-        
+
+        await validacaoNotas(notas);
+
+        const media = notas.reduce((accumulator, currentValue) => accumulator + currentValue) / notas.length;
+        const resultado = media > 6 ? `Olá ${nome} voce foi aprovado(a) com média ${media}.` : `Olá ${nome}, voce foi reprovao(a) com média de ${media}`;
+        // if (media > 6) {
+        //     Aprovacao = `Você foi aprovado(a)`
+        // } else {
+        //     Aprovacao = `Voce foi reprovada`
+        // }
+        res.status(201).json({ message: resultado });
+
     } catch (error) {
-       console.error(error);
-        res.status(500).json({ message: 'Ocorreu um erro ao processar', errorMessage: error.message}) 
+        console.error(error);
+        res.status(500).json({ message: 'Ocorreu um erro ao processar', errorMessage: error.message })
     }
 })
 app.listen(PORT, () => {
     console.log(`Server rodando na: http://localhost:${PORT}`);
 })
+
